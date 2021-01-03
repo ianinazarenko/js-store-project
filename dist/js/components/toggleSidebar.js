88a7ed3872a874cff5053fa162726c4aa7858033
Ianina <ynzrnk@yandex.ru>
2021-01-03 19:52:48 +0300

    Files structure is changed and HTML for Featured is added

--- dist/js/components/toggleSidebar.js
+++ dist/js/components/toggleSidebar.js
@@ -1,7 +1,7 @@
 import { getElement } from "../utilities.js";
 
 const toggleBtn = getElement(".navbar-toggle-btn");
-const closeSidebarBtn = getElement(".close-btn-sidebar");
+const closeSidebarBtn = getElement(".sidebar-close-btn");
 const sidebar = getElement(".sidebar-overlay");
 
 toggleBtn.addEventListener("click", (e) => {
